d0334ae2fa913aa2970586f2e72660500f77c199
Mahendran Jayachandra <mahendran_res@yahoo.com>
2023-10-17 18:56:33 +0530

    ssmparameters

---
+++ CreateSSMParameters/main.tf
@@ -0,0 +1,11 @@
+resource "aws_ssm_parameter" "params" {
+  foreach = var.paramslist
+  name  = paramslist.value["name"]
+  type  = paramslist.value["type"]
+  value = paramslist.value["value"]
+}
+
+variable "paramslist" {
+param1 = {name = "demo1",type="String",value="test1"},
+param2 = {name = "demo2",type="String",value="test2"}
+}
