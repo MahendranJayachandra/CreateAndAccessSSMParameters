44613300dc449d4676b1b3ef255a4de738019b05
mahendran jayachandra <mahendran_res@yahoo.com>
2023-10-30 12:37:39 +0530

    frontend setup with ssm parameters

--- frontend/main.tf
+++ frontend/main.tf
@@ -1,8 +1,8 @@
 data "aws_ami" "devops" {
-  executable_users = [973714476881]
+  #executable_users = [973714476881]
   most_recent = true
   name_regex       = "devops-practice"
-  # owners = ["973714476881"] # Canonical
+  owners = [973714476881] # Canonical
 }
 
 resource "aws_instance" "web" {
